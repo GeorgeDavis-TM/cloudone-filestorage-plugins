4ab294aacf2e85d71ff92c550bb6884ad3b4dda1
George Davis <george_davis@trendmicro.com>
2022-07-28 09:34:24 -0400

    build: Updating code for email notifications

--- post-scan-actions/gcp-python-email-notification/main.py
+++ post-scan-actions/gcp-python-email-notification/main.py
@@ -1,67 +1,108 @@
-import json
 import os
-import sendmail
+import json
+import base64
+import pubsub_sendmail
 
-def lambda_handler(event, context):
+def get_gcp_project_id(resource_name):
+    return str(resource_name).split("/")[1]
 
-    mailFrom      = os.environ.get('MAIL_FROM', '').strip()
-    mailTo        = os.environ.get('MAIL_TO', '').strip()
-    mailSubject   = os.environ.get('MAIL_SUBJECT', '').strip()
-    mailServer    = os.environ.get('MAIL_SERVER', '').strip()
-    mailLocalHost = os.environ.get('MAIL_LOCAL_HOST', '').strip()
-    mailForceTls  = os.environ.get('MAIL_FORCE_TLS', '').strip()
-    mailDebug     = os.environ.get('MAIL_DEBUG', '').strip()
-    # charset = "UTF-8"
+def get_bucket_name_from_file_url(file_url):
+    return str(file_url.split("/")[-2:][0])
 
-    mailDict = {}
-    mailDict.update({"mailFrom": mailFrom})
-    mailDict.update({"mailTo": mailTo})
-    mailDict.update({"mailSubject": mailSubject})
-    mailDict.update({"mailServer": mailServer})
-    mailDict.update({"mailLocalHost": mailLocalHost})
-    mailDict.update({"mailForceTls": mailForceTls})
-    mailDict.update({"mailDebug": mailDebug})
+def get_file_name_from_file_url(file_url):
+    return str(file_url.split("/")[-1:][0])
 
-    for record in event['Records']:
+def build_file_metadata_url(project_id, bucket_name, file_name):
+    return "https://console.cloud.google.com/storage/browser/_details/" + bucket_name + "/" + file_name + "?project=" + project_id
 
-        # Analyze the message and look for findings
-        message = json.loads(record['Sns']['Message'])
-        findings = message['scanning_result'].get('Findings')
+def main(event, context):
 
-        if findings:
+    try:
 
-            # body_text = '''\
-            #     CloudOne FSS Email Notification
-            #     File URL: {file_url}\
-            #     '''.format(file_url=str(message['file_url']))
-            # for finding in message['scanning_result']['Findings']:
-            #     body_text = body_text + '''\
-            #         Malware: {malware}
-            #         Type: {type}\
-            #         '''.format(malware=str(finding.get('malware')), type=str(finding.get('type')))
+        print(f'Context: {context}')
+        print(f'Event: {event}')
+        base64_data = event.get('data', '')
+        base64_bytes = base64_data.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        message = json.loads(message_bytes.decode('ascii'))
+        print(f'Message: {message}')
 
-            body_html = '''\
-                <html><head></head><body><h1>CloudOne FSS Email Notification</h1><p>
-                <p><b>File URL: </b>{file_url}</p>\
-                '''.format(file_url=str(message['file_url']))
-            for finding in message['scanning_result']['Findings']:
-                body_html = body_html + '''\
-                    <p><b>Malware: </b>{malware}</p>
-                    <p><b>Type: </b>{type}</p>
-                    </html></body></p>\
-                    '''.format(malware=str(finding.get('malware')), type=str(finding.get('type')))
+        print("""This Function was triggered by messageId {} published at {} to {}""".format(context.event_id, context.timestamp, context.resource))
 
-            mailDict.update({"mailMessageBody": body_html})
+        mailFrom      = os.environ.get('MAIL_FROM', '').strip()
+        mailTo        = os.environ.get('MAIL_TO', '').strip()
+        mailSubject   = os.environ.get('MAIL_SUBJECT', '').strip()
+        mailServer    = os.environ.get('MAIL_SERVER', '').strip()
+        mailLocalHost = os.environ.get('MAIL_LOCAL_HOST', '').strip()
+        mailForceTls  = os.environ.get('MAIL_FORCE_TLS', '').strip()
+        mailDebug     = os.environ.get('MAIL_DEBUG', '').strip()
+        # charset = "UTF-8"
 
-            # Try to send the email.
-            try:
-                response = sendmail.pubsub_sendmail(mailDict=mailDict, context=context)
-            # Display an error if something goes wrong.
-            except Exception as e:
-                print(e)
-                return
-            else:
-                print("Email sent! Message ID:" + response['MessageId'])
-                return
+        mailDict = {}
+        mailDict.update({"mailFrom": mailFrom})
+        mailDict.update({"mailTo": mailTo})
+        mailDict.update({"mailSubject": mailSubject})
+        mailDict.update({"mailServer": mailServer})
+        mailDict.update({"mailLocalHost": mailLocalHost})
+        mailDict.update({"mailForceTls": mailForceTls})
+        mailDict.update({"mailDebug": mailDebug})
+
+        if message:
+            # Message details from the Pub/Sub topic publish event
+            findings = message['scanning_result'].get('Findings')
+
+            if findings:
+
+                project_id = get_gcp_project_id(resource_name=context.resource)
+                bucket_name = get_bucket_name_from_file_url(file_url=str(message['file_url']))
+                file_name = get_file_name_from_file_url(file_url=str(message['file_url']))
+
+                # body_text = '''\
+                #     CloudOne FSS Email Notification
+                #     File URL: {file_url}\
+                #     '''.format(file_url=str(message['file_url']))
+                # for finding in message['scanning_result']['Findings']:
+                #     body_text = body_text + '''\
+                #         Malware: {malware}
+                #         Type: {type}\
+                #         '''.format(malware=str(finding.get('malware')), type=str(finding.get('type')))
+
+                body_html = '''\
+                        <html><head></head><body><h1>Cloud One FSS Email Notification</h1><p>
+                        <p><b>GCP Project ID: </b>{project_id}</p>
+                        <p><b>GCP Bucket Name: </b>{bucket_name}</p>
+                        <p><b>File Name: </b>{file_name}</p>
+                    '''.format(
+                        project_id=project_id,
+                        bucket_name=bucket_name,
+                        file_name=file_name
+                    )
+                for finding in message['scanning_result']['Findings']:
+                    body_html = body_html + '''\
+                        <p><b>Malware Name(s): </b>{malware}</p>
+                        <p><b>Malware Type(s): </b>{type}</p>
+                        <p><b>File URL: </b>{file_metadata_url}</p>
+                        </html></body></p>\
+                        '''.format(
+                            malware=str(finding.get('malware')),
+                            type=str(finding.get('type')),
+                            file_metadata_url=str(build_file_metadata_url(project_id=project_id, bucket_name=bucket_name, file_name=file_name))
+                        )
+
+                mailDict.update({"mailMessageBody": body_html})
+
+                # Try to send the email.
+                try:
+                    response = pubsub_sendmail.pubsub_sendmail(mailDict=mailDict, context=context)
+                # Display an error if something goes wrong.
+                except Exception as e:
+                    print(e)
+                    return
+                else:
+                    print("Email sent! Message ID:" + response['MessageId'])
+                    return
+
+    except Exception as e:
+        print("Error: ", str(e))
 
     print("Nothing done.")
